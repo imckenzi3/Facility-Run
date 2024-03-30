783c79146b4e2e20a513fd7369073f14342ea9f0
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2024-03-30 14:34:33 -0700

    Bug Fixes

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -1,5 +1,7 @@
+@icon("res://assets/characters/icon.svg")
+
 extends CharacterBody2D
-#class_name Character, "res://cow.png"
+class_name Character
 
 const speed = 450
 const jump_power = -1200
@@ -17,12 +19,24 @@ var current_jumps = 1
 
 #shovel - ref to node
 @onready var shovel: Node2D = get_node("Shovel")
+@onready var shovel_hitbox: Area2D = get_node("Shovel/Node2D/Sprite2D/Hitbox")
 @onready var shovel_animation_player: AnimationPlayer = shovel.get_node("ShovelAnimationPlayer")
+
+@export var hp: int = 2
+#take damage
+#variable to change the state when the character takes damage
+@onready var state_machine: Node = get_node("FiniteStateMachine")
+
+#move
+const FRICTION: float = 0.15
 
 
 #loop over and over
-func _physics_process(delta):
+func _physics_process(_delta: float) -> void:
 	var input_dir: Vector2 = input()
+	
+	#for move
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
 	
 	#if we want player to move have to call accerlation, add friction, add player movement
 	if input_dir != Vector2.ZERO: 
@@ -43,6 +57,11 @@ func _physics_process(delta):
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 	#update the rotation of the shovel using the angle of the mouse direction
 	shovel.rotation = mouse_direction.angle()
+	
+	#after chaning the sword rotation, set the knockback direction of the 
+	#hitbox with the mouse direction
+	shovel_hitbox.knockback_direction = mouse_direction
+	
 	if shovel.scale.y == 1 and mouse_direction.x < 0:
 		shovel.scale.y = -1
 	elif shovel.scale.y == -1 and mouse_direction.x > 0:
@@ -87,9 +106,21 @@ func jump():
 	if is_on_floor():
 		current_jumps = 1
 
-		
+#Take Damage
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	state_machine.set_state(state_machine.states.hurt)
+	velocity += dir * force
+	
 	
 #play animations
 func play_animation():
 	pass
+
+#move
+var mov_direction: Vector2 = Vector2.ZERO
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * acc
+	velocity = velocity.limit_length(speed)
 
