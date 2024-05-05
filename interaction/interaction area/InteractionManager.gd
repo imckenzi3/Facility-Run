30ca7c78e64f4e4f5e728395eaf21d50523e95db
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2024-05-04 18:04:44 -0700

    Adding Shadows

--- interaction/interaction area/InteractionManager.gd
+++ interaction/interaction area/InteractionManager.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 #@onready var player = get_tree().get_first_node_in_group("player") #get player
-@onready var player: CharacterBody2D = get_tree().current_scene.get_node("player") #ref to player node
+@onready var player = $Player #get player
+#@onready var player: CharacterBody2D = get_tree().current_scene.get_node("player") #ref to player node
 
 @onready var label = $Label #get label
 
