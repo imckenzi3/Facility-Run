d9e0c18923fab1a4e558ebd411e1e4788ea23872
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2024-05-05 09:50:48 -0700

    Added Npc Idle Animation

--- characters/npcs/test_npc.gd
+++ characters/npcs/test_npc.gd
@@ -4,15 +4,18 @@ extends CharacterBody2D
 @onready var sprite = $AnimatedSprite2D
 @onready var speech_sound 
 
-const lines: Array[String] = [
+@onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+
+const lines: Array[String] = [ #dialog text
 	"Hi, welcome to the facility",
 	"Watch your step",
 	"Press space to end this conversation"
-	
 ]
 
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
+	
+	animation_player.play("idle") #play idle animation
 
 func _on_interact():
 	DialogManager.start_dialog(global_position, lines, speech_sound)
